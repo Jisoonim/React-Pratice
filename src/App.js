e0b2d15a7dc7656d7ddbdfde2380fded51ba84ad
Jisoonim <bubu224@naver.com>
2020-01-06 18:30:33 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SWTest from './components/day03/SWTest';
 
 function App() {
 
-  const arr = [  
+  const arr1 = [  
     {title:"손흥민이 잉글랜드 프리미어리그에서 소속한 팀 이름은?", answer:"토트넘" },
     {title:"레드벨벳 소속 가수가 아닌사람은?(ex.예리 / 아이린 / 슬기 / 조이 / 유정", answer:"유정" },
     {title:"2020년은 경자년이다. 12간지 중 해당하는 동물은? ", answer:"쥐" },
@@ -18,7 +18,13 @@ function App() {
     {title:"프로그래밍 언어가 아닌 것을 고르시오.(ex.Java / Tomcat / React / Ruby / python ", answer:"Tomcat" }, 
  ]
 
- 
+    const arr2 = [  
+      {title:"ZZQuiz1...", answer:"1" },
+      {title:"ZZQuiz2...", answer:"2" },
+      {title:"ZZQuiz3...", answer:"3" },
+      {title:"ZZQuiz4...", answer:"4" },
+      {title:"ZZQuiz5...", answer:"5" }, 
+    ]
 
   return (
     <div className="App">
@@ -34,7 +40,7 @@ function App() {
 
 
 
-        {/* <QuizComponent arr={arr}></QuizComponent> //퀴즈문제출력
+        {/* <QuizComponent arr={arr1}></QuizComponent> //퀴즈문제출력
 
 
         <CountHookComponent></CountHookComponent>*/}
